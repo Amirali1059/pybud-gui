2c2b29e917c2454b92d4e2602c18df584740f997
AmirAli mim <amiralimiim@gmail.com>
2024-04-12 13:39:25 +0330

    readme update

---
+++ examples/build-your-first-dialoge.py
@@ -0,0 +1,15 @@
+# import the main dialog class
+from pybud.gui.dialog import AutoDialog
+# import widgets
+from pybud.gui.widgets import WidgetLabel
+# import types
+from pybud.deftypes import Point, Size
+
+# build the main dialoge, set width and background color
+d = AutoDialog(width=60, background_color=(90, 90, 250))
+# add the main widget, set text, width and position
+d.add_widget(WidgetLabel("Hello world!", pos=Point(y=1), size=Size(w=60)))
+# show the dialoge
+d.show()
+# after the dialog closes
+print("closed!")
